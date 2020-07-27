854a25d016a70707689e030c8deafc788f967255
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2020-07-27 15:27:08 +0500

    Introduce other default params

--- src/Genetics/Defaults.hs
+++ src/Genetics/Defaults.hs
@@ -1,6 +1,14 @@
 module Genetics.Defaults where
 
 
+-- * Population
+
+populationCoefficient :: Int
+populationCoefficient = 1
+
+populationSize :: Integer
+populationSize = fromIntegral populationCoefficient * 150
+
 -- * NEAT Parameters
 
 c1 :: Double
@@ -19,5 +27,42 @@ deltaT = 3.0
 delta :: Double -> Double -> Double -> Double -> Double
 delta e d n w = c1*e/n + c2*d/n + c3*w
 
+
 stagnantGenerationsLimit :: Integer
 stagnantGenerationsLimit = 15
+
+
+weightMutationProbability :: Double
+weightMutationProbability = 0.8
+
+randomWeightMutationProbability :: Double
+randomWeightMutationProbability = 0.1
+
+popMutationFactor :: Double
+popMutationFactor = 0.25
+
+
+-- | Maximum difference in weights when uniform perturbation applied.
+--   Value was chosen randomly because there were no example value
+--   in docs.
+perturbationFactor :: Double
+perturbationFactor = 0.1
+
+geneDisableProbability :: Double
+geneDisableProbability = 0.75
+
+interspeciesMatingRate :: Double
+interspeciesMatingRate = 0.0001
+
+smNodeAdditionProbability :: Double
+smNodeAdditionProbability = 0.03
+
+smLinkAdditionProbability :: Double
+smLinkAdditionProbability = 0.05
+
+lgLinkAdditionProbability :: Double
+lgLinkAdditionProbability = 0.3
+
+
+solutionTimeout :: Int
+solutionTimeout = 1 * 1000 * 1000
